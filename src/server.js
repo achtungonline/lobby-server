75c3338b3cb775cd10db2f7faf2398b108ab2fb7
Erik Odenman <erik.odenman@gmail.com>
2016-09-14 16:05:15 +0200

    added simple lobby support for multiplayer games

--- src/server.js
+++ src/server.js
@@ -2,43 +2,154 @@ var io = require('socket.io')({ serveClient: false });
 
 var Match = require("core/src/core/match.js");
 var random = require("core/src/core/util/random.js");
-var forEach = require("core/src/core/util/for-each.js");
+var gameStateFunctions = require("core/src/core/game-state-functions.js");
 
 var ServerGame = require("./server-game.js");
 
-io.on('connection', function(socket){
+var PLAYERS_PER_ROOM = 2;
+var nextRoomId = 0;
+var rooms = [];
+
+function Room() {
+    var roomId = nextRoomId + "";
+    nextRoomId++;
+    var nextPlayerId = 0;
+    var players = [];
     var match = null;
+    var game = null;
     var serverGame = null;
 
-    socket.on("match_config", function(matchConfig) {
+    function addPlayer(socket) {
+        if (players.length >= PLAYERS_PER_ROOM) {
+            throw Error("Adding player to full room");
+        }
+        var player = {
+            id: nextPlayerId + "",
+            socket: socket,
+            ready: false
+        };
+        nextPlayerId++;
+        players.push(player);
+        return player.id;
+    }
+
+    function removePlayer(playerId) {
+        var index = players.findIndex(p => p.id === playerId);
+        players.splice(index, 1);
+    }
+
+    function playerReady(playerId) {
+        console.log(playerId + " is ready");
+        var player = players.find(p => p.id === playerId);
+        player.ready = true;
+        if (players.length === PLAYERS_PER_ROOM && players.every(p => p.ready)) {
+            startGame();
+        }
+    }
+
+    function startGame() {
+        var matchConfig = {
+            players: players.map(p => ({id: p.id})),
+            map: gameStateFunctions.createMapSquare("Square 500", 500),
+            maxScore: 5*(players.length - 1)
+        };
         match = Match({ matchConfig });
-    });
-    socket.on("start_game", function() {
         var seed = random.generateSeed();
-        var game = match.prepareNextGame(seed);
-        socket.emit("start_game", game.gameState);
+        game = match.prepareNextGame(seed);
+        players.forEach(player => {
+            player.socket.emit("start_game", game.gameState);
+        });
         serverGame = ServerGame({
             game,
             onGameUpdate: function(update) {
-                socket.emit("game_updated", update);
+                players.forEach(player => {
+                    player.socket.emit("game_updated", update);
+                });
             },
             onGameOver: function() {
-                socket.emit("game_over");
-                console.log("Game over");
+                players.forEach(player => {
+                    player.socket.emit("game_over");
+                });
+                console.log("Game " + roomId + " over");
+                removeRoom(roomId);
             }
         });
         serverGame.start();
-    });
-    socket.on("player_steering", function(data) {
-        forEach(data, function(steering, playerId) {
+    }
+
+    function setPlayerSteering(playerId, steering) {
+        if (serverGame) {
             serverGame.setPlayerSteering(playerId, steering);
-        })
-    });
-    socket.on("disconnect", function() {
-        console.log("Client disconnected");
+        }
+    }
+
+    function isFull() {
+        return players.length === PLAYERS_PER_ROOM;
+    }
+
+    return {
+        addPlayer,
+        id: roomId,
+        isFull,
+        playerReady,
+        removePlayer,
+        setPlayerSteering
+    }
+}
+
+function addRoom() {
+    var room = Room();
+    rooms.push(room);
+    console.log("Created room " + room.id);
+    return room;
+}
+
+function getRoom(roomId) {
+    return rooms.find(r => r.id === roomId);
+}
+
+function getVacantRoomId() {
+    if (rooms.length === 0 || rooms[rooms.length - 1].isFull()) {
+        return addRoom().id;
+    } else {
+        return rooms[rooms.length - 1].id;
+    }
+}
+
+function removeRoom(roomId) {
+    var index = rooms.findIndex(r => r.id === roomId);
+    rooms.splice(index, 1);
+    console.log("Room " + roomId + " removed");
+}
+
+io.on('connection', function(socket){
+    console.log("Client connected: " + socket.id);
+    var roomId = getVacantRoomId();
+    var playerId = getRoom(roomId).addPlayer(socket);
+    socket.emit("player_id", playerId);
+    console.log("Added player " + playerId + " to room " + roomId);
+
+    socket.on("ready", () => {
+        var room = getRoom(roomId);
+        if (room) {
+            room.playerReady(playerId);
+        }
     });
 
-    console.log("Client connected");
+    socket.on("player_steering", steering => {
+        var room = getRoom(roomId);
+        if (room) {
+            room.setPlayerSteering(playerId, steering[playerId]);
+        }
+    });
+
+    socket.on("disconnect", function() {
+        var room = getRoom(roomId);
+        if (room) {
+            room.removePlayer(playerId);
+        }
+        console.log("Client disconnected: " + socket.id);
+    });
 });
 io.listen(3000);
 console.log("Server started");
